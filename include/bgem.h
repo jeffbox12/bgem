8a4c1e18b9023445070f5aa182a89a8760af77ea
Cătălin Gabriel Drăghiță <catalingabriel.bgem@outlook.com>
2026-05-09 09:22:04 +0200

    Implement product identification assets

---
+++ include/bgem.h
@@ -0,0 +1,15 @@
+/*
+ *  MIT License
+ *  Copyright (c) 2026 Cătălin Gabriel Drăghiță
+ */
+
+#ifndef BGEM_H
+#define BGEM_H
+
+#define BGEM__WIDE(s)   L##s
+#define BGEM_WIDE(s)    BGEM__WIDE(s)
+
+#define BGEM_APP_NAME   "Project Bluegem"
+#define BGEM_APP_NAME_W BGEM_WIDE(BGEM_APP_NAME)
+
+#endif // BGEM_H
